73ca68b74a15c67da75c813dc8ea07f53649a881
Oksana Lopatkina <oksana_lopatkina@epam.com>
2023-02-26 14:38:37 +0300

    import url is implemented

--- src/constants/api-paths.ts
+++ src/constants/api-paths.ts
@@ -1,7 +1,7 @@
 export const API_PATHS = {
 	product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 	order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-	import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+	import: 'https://fgrijtqa2k.execute-api.us-east-1.amazonaws.com/dev',
 	bff: 'https://qrc6eg3r7d.execute-api.us-east-1.amazonaws.com/dev',
 	cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
